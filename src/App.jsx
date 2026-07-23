b3eb14382c09eb3b2f8d6e37ab3ab54d2917c1c6
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-07-23 13:37:35 +0200

    feat: complete header and footer layout with responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Recipes from "./pages/Recipes";
-import Admin from "./pages/Admin";
-import "./App.scss";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app-container">
+      <Header />
+      <main className="main-content">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
